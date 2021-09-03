a22eb2168be841df3a28e80fac4f5cb130b2845a
Alex M <milesAwayAlex@gmail.com>
2021-09-02 20:47:30 -0700

    Add the regexes schema

--- database/loader.js
+++ database/loader.js
@@ -2,21 +2,24 @@ require('dotenv').config();
 const { Client } = require('pg');
 const { readFile, readdir } = require('fs/promises');
 
-const dbClient = new Client(process.env.DBKEY);
-dbClient.connect();
-
 const getQueries = (arr) =>
-  Promise.all(arr.map((filename) => readFile(dir + '/' + filename, 'utf-8')));
+  Promise.all(
+    arr.map((filename) => {
+      console.log('Reading: ', filename);
+      return readFile(dir + '/' + filename, 'utf-8');
+    })
+  );
 
 const runQueries = (queries) =>
   Promise.all(queries.map((sql) => dbClient.query(sql)));
 
+const dbClient = new Client(process.env.DBKEY);
 const dir = 'database/schema';
 
 console.log('Running the schema files from', dir);
 
-readdir(dir)
-  .then((arr) => getQueries(arr))
+Promise.all([dbClient.connect(), readdir(dir)])
+  .then(([nothing, arr]) => getQueries(arr))
   .then((queries) => runQueries(queries))
   .then(() => console.log('Finished'))
   .finally(() => dbClient.end());
