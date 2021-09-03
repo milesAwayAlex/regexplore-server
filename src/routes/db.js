b98fbd5eea4a0954c8f9ebc3d5e0ada43ff183be
Alex M <milesAwayAlex@gmail.com>
2021-09-03 07:33:35 -0700

    Fix error handling in the db router

--- src/routes/db.js
+++ src/routes/db.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 
 module.exports = (db) => {
   // this is used for the db smoke test
-  router.get('/', async (req, res) => {
+  router.get('/', async (req, res, next) => {
     try {
       const { rows } = await db.query('SELECT NOW() AS "time"');
       res.json(rows);
     } catch (e) {
-      console.error(e);
+      next(e);
     }
   });
 
   // this is used to wrap up the test suite
-  router.get('/disconnect', async (req, res) => {
+  router.get('/disconnect', async (req, res, next) => {
     try {
       await db.end();
       res.send('disconnected from the database');
     } catch (e) {
-      console.error(e);
+      next(e);
     }
   });
   return router;
