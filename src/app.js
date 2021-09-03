152498d658ee79c87160158205d2231eabdac6ce
Alex M <milesAwayAlex@gmail.com>
2021-09-03 07:31:04 -0700

    Set up json middleware and the tags router

--- src/app.js
+++ src/app.js
@@ -3,13 +3,20 @@ const { Client } = require('pg');
 
 // routers
 const dbTestRouter = require('./routes/db');
+const tagsRouter = require('./routes/tags');
 
 const app = express();
+app.use(express.json());
+
 const dbClient = new Client(process.env.DBKEY);
 dbClient.connect();
 
+// TODO protect!
 app.get('/', (req, res) => res.send('here be server..'));
 
+// TODO protect!!!
 app.use('/db-test', dbTestRouter(dbClient));
+
+app.use('/tags', tagsRouter(dbClient));
 
 module.exports = { app };
