e830a11b4a09db8ec3ce5fcab2e02216e13c3610
Alex M <milesAwayAlex@gmail.com>
2021-09-02 22:00:02 -0700

    Add the seeds for the tags, adjust the loader

--- database/loader.js
+++ database/loader.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Client } = require('pg');
 const { readFile, readdir } = require('fs/promises');
 
-const getQueries = (arr) =>
+const getQueries = (arr, dir) =>
   Promise.all(
     arr.map((filename) => {
-      console.log('Reading: ', filename);
-      return readFile(dir + '/' + filename, 'utf-8');
+      console.log(`Reading ${dir}/${filename}`);
+      return readFile(`${dir}/${filename}`, 'utf-8');
     })
   );
 
@@ -14,12 +14,19 @@ const runQueries = (queries) =>
   Promise.all(queries.map((sql) => dbClient.query(sql)));
 
 const dbClient = new Client(process.env.DBKEY);
-const dir = 'database/schema';
+const schemaDir = 'database/schema';
+const seedsDir = 'database/test-seeds';
 
-console.log('Running the schema files from', dir);
+console.log('Running the schema files from', schemaDir);
 
-Promise.all([dbClient.connect(), readdir(dir)])
-  .then(([nothing, arr]) => getQueries(arr))
+Promise.all([dbClient.connect(), readdir(schemaDir)])
+  .then(([nothing, arr]) => getQueries(arr, schemaDir))
   .then((queries) => runQueries(queries))
-  .then(() => console.log('Finished'))
+  .then(() =>
+    console.log('Finished with the schema, running the seeds from', seedsDir)
+  )
+  .then(() => readdir(seedsDir))
+  .then((arr) => getQueries(arr, seedsDir))
+  .then((queries) => runQueries(queries))
+  .then(() => console.log('Finished with the seeds'))
   .finally(() => dbClient.end());
